411177db7188574fa42e869a3d85324b10b03111
Saad Equbal <90771672+ssequbal@users.noreply.github.com>
2022-07-22 12:57:04 +0530

    Main Driver

---
+++ Driver.java
@@ -0,0 +1,14 @@
+/*
+ *  Another simple GUI demonstration using swing.
+ *  Here the GridSquare class extends JPanel and
+ *  holds its coordinates in the grid as attributes.
+ *  @author mhatcher
+ */
+public class Driver
+{
+	public static void main(String[] args)
+	{
+		// create a new GUI window
+		mindTheGap demo = new mindTheGap();
+	}
+}
